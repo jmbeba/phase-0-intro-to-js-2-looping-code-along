0fb351dd327e82d4ce7902bbd1a263f50eb757c3
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-02 12:42:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let myArr = [];
+function writeCards(names, eventName){
+    for(let i=0; i<names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        myArr.push(message);
+    }
+
+    return myArr;
+}
+
+function countDown(num){
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+}
